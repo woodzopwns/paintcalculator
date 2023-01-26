392ad8427a2b75ded713893613182ccd5cb15ed7
woodzopwns <woodzopwns61@gmail.com>
2023-01-26 11:28:36 +0000

    -renamed House.py to house.py for standard -added user input validation reuse with user_input() -reformatted main.py to reuse input validation to de-bloat code

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
-from House import Paint, Wall, Room, House
+from house import Paint, Wall, Room, House
+
+
+def user_input(is_string):
+    retrieve_input = None
+    valid = False
+    while not valid:
+        try:
+            if is_string:
+                retrieve_input = str(input())
+                valid = True
+            else:
+                retrieve_input = float(input())
+                valid = True
+        except ValueError:
+            print(f'''Sorry! That's not the answer I was expecting!\n''')
+    return retrieve_input
+
 
 # Introduction
 print('Hello! Welcome to the house paint price calculator\n'
@@ -6,55 +23,45 @@ print('Hello! Welcome to the house paint price calculator\n'
       'internal or external :).\n')
 
 # Take user input on number of rooms
-valid = False
-while not valid:
-    try:
-        print('How many rooms will be in your house?')
-        number_rooms = int(input())
-        valid = True
-    except ValueError:
-        print('Sorry! I need a number!\n')
+print('How many rooms will be in your house?')
+number_rooms = user_input(False)
 
 # Take user input on paint details
-valid = False
-while not valid:
-    try:
-        print('How much does your paint cost per can?')
-        paint_cost = int(input())
-        print('How many metres does your paint cover per can?')
-        paint_coverage = int(input())
-        valid = True
-    except ValueError:
-        print('Sorry! I need a number!\n')
+print('How much does your paint cost per can?')
+paint_cost = user_input(False)
+print('How many metres does your paint cover per can?')
+paint_coverage = user_input(False)
 
 # Making a list of rooms to be added to the house later
 rooms = []
-for room in range(number_rooms):
+for room in range(int(number_rooms)):
     rooms.append(Room())
 
 # I don't know what the fuck this is
 for itx, room in enumerate(rooms):
-    print(f'How many walls are in room {itx+1}?')
+    print(f'How many walls are in room {itx + 1}?')
     number_walls = int(input())
+
     for wall in range(number_walls):
-        print(f'How wide is wall {wall+1}?')
-        width = int(input())
-        print(f'How tall is wall {wall+1}?')
-        height = int(input())
+        print(f'How wide is wall {wall + 1}?')
+        width = user_input(False)
+        print(f'How tall is wall {wall + 1}?')
+        height = user_input(False)
         new_wall = Wall(width, height)
-        print(f'Are there any obstructions on wall {wall+1}? [Y/N]')
-        if str(input()).upper() == "Y":
+        print(f'Are there any obstructions on wall {wall + 1}? [Y/N]')
+
+        if user_input(True).upper() == "Y":
             print(f'Is it circular or square? [C/S]')
-            obstruction_type = str(input())
-            if obstruction_type.upper() == "S":
+            obstruction_type = user_input(True).upper()
+            if obstruction_type == "S":
                 print(f'What is the obstruction height?')
-                obstruction_height = int(input())
+                obstruction_height = user_input(False)
                 print(f'And the width?')
-                obstruction_width = int(input())
+                obstruction_width = user_input(False)
                 new_wall.add_square_obstruction(height, width)
-            elif obstruction_type.upper() == "C":
+            elif obstruction_type == "C":
                 print(f'What is the obstruction radius?')
-                obstruction_radius = int(input())
+                obstruction_radius = user_input(False)
                 new_wall.add_circular_obstruction(obstruction_radius)
         else:
             rooms[itx].add_wall(new_wall)
@@ -70,3 +77,13 @@ price = blue_paint.calculate_price(house.get_house_area())
 
 # Final price to 2 decimals as per currency standards
 print(f'Your final price is: £%.2f' % price)
+
+# ___________________________ TO DO (in order) ___________________________
+# User input validation as a function (take str or int)
+# Paint type input functionality
+# Full input functionality
+# Unit testing
+# E2E testing
+# Comment code more comprehensively
+# Saving data into CSV file
+# Consider adding a user interface functionality
